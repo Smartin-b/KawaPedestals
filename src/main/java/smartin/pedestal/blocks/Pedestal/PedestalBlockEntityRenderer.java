2f398af0df29b2981e26ec499720b0b66add39dd
MartinLaptop <martinbachmor@gmail.com>
2023-01-02 22:27:18 +0100

    medievalweapon support

--- src/main/java/smartin/pedestal/blocks/Pedestal/PedestalBlockEntityRenderer.java
+++ src/main/java/smartin/pedestal/blocks/Pedestal/PedestalBlockEntityRenderer.java
@@ -42,15 +42,15 @@ public class PedestalBlockEntityRenderer implements BlockEntityRenderer<Pedestal
             matrix.translate(0.0D, 0.35D, 0.0D);
         matrix.translate(0.5D, 0.6D, 0.5D);
         matrix.scale(0.80F, 0.80F, 0.80F);
-        switch (tile.getCachedState().get(HorizontalFacingBlock.FACING)) {
-            case WEST, EAST -> this.rotateItem(matrix, 180, 90f, -45f); // default values
-            case NORTH, SOUTH -> this.rotateItem(matrix, 180, 180f, -45f);
-        }
         SwordJson.TranslationObject translationObject = Pedestal.SwordJsons.get("pedestal").get(Registry.ITEM.getId(stack.getItem()).toString());
         if(translationObject !=null){
             matrix.translate(translationObject.translation[0],translationObject.translation[1],translationObject.translation[2]);
             matrix.scale(translationObject.scale[0].floatValue(),translationObject.scale[1].floatValue(),translationObject.scale[2].floatValue());
             this.rotateItem(matrix,translationObject.rotation[0].floatValue(),translationObject.rotation[1].floatValue(),translationObject.rotation[2].floatValue());
+        }
+        switch (tile.getCachedState().get(HorizontalFacingBlock.FACING)) {
+            case WEST, EAST -> this.rotateItem(matrix, 180, 90f, -45f); // default values
+            case NORTH, SOUTH -> this.rotateItem(matrix, 180, 180f, -45f);
         }
         renderer.renderItem(stack, ModelTransformation.Mode.FIXED, combinedLight, combinedOverlay, matrix, buffer, 1);
 
